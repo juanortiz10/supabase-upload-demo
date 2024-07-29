5a9b38a456002cf56265ceeaf6f4fd97fd9d21c6
Juan Ortiz Jr <juanjo.jjo94@gmail.com>
2024-07-29 16:22:20 -0600

    Add example without best practices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,145 @@
+'use client';
+
 import Image from "next/image";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import useSWR from "swr";
 
-export default function Home() {
+import createClient from "../utils/supabase/client";
+
+const Home = () => {
+  const supabase = createClient();
+  const { data, isLoading } = useSWR('users', async () => await supabase.from('users').select('*').eq('id', 1).single());
+  const user = data?.data || {};
+
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [file, setFile] = useState<File>();
+  const [avatarUrl, setAvatarUrl] = useState<string>();
+
+  
+  useEffect(() => {
+    if (user.email !== email) {
+      setEmail(user.email);
+    }
+
+    if (user.full_name !== fullName) {
+      setFullName(user.full_name);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+  
+  useEffect(() => {
+    async function downloadImage(path: string) {
+      try {
+        const { data, error } = await supabase.storage.from('profile-upload-test').createSignedUrl(`public/${path}`, 60);
+
+        if (error) {
+          throw error;
+        }
+ 
+        setAvatarUrl(data.signedUrl);
+      } catch (error) {
+        console.log('Error downloading image: ', error)
+      }
+    }
+
+    if (!!user.avatar_url) {
+      downloadImage(user.avatar_url);
+    }
+  }, [user.avatar_url, supabase]);
+   
+  const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    let filePath = user.avatar_url;
+
+    if (file) {
+      const fileExt = file.name.split('.').pop()
+      
+      filePath = `1-${Math.random()}.${fileExt}`
+
+      const { error } = await supabase.storage.from('profile-upload-test').upload(`public/${filePath}`, file, {
+        cacheControl: '3600',
+        upsert: false
+      });
+
+      if (error) {
+        console.log('Error uploading file: ', error);
+        return;
+      }
+    }
+ 
+    const result = await supabase.from('users').update({
+      full_name: fullName,
+      email: email,
+      avatar_url: filePath,
+    }).eq('id', 1); // HARD USER
+
+    if (result.error) {
+      console.log('Error updating user: ', result.error);
+      return;
+    }
+
+    // TODO - server action here
+  };
+
+  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event?.target?.files?.[0]) return;
+
+    const { target: { files } } = event;
+    setFile(files[0]);
+  };
+
+  // TODO - Better flag for loading
+  if (isLoading) {
+    return (
+      <div className="flex bg-black justify-center items-center py-10">
+        <h3 className="text-white">Cargando...</h3>
+      </div>
+    );
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+    <div className="flex bg-black justify-center items-center py-10">
+      <form className="flex flex-col" onSubmit={handleFormSubmit}>
+        <h3 className="text-lg text-black">Perfil</h3>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={avatarUrl || ''} // TODO default image here
+          alt="profile-photo"
+          className="rounded-full w-48 h-48"
+          width={120}
+          height={120}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <label htmlFor="file" className="text-black">Subir foto</label>
+        <input type="file" id="file" name="file" onChange={handleImageChange} accept="image/*" />
+        <label className="flex flex-col mb-4 mt-4">
+          Nombre
+          <input
+            type="text"
+            className="text-black px-2 py-1 rounded-sm"
+            value={fullName}
+            onChange={({ target: { value }}: ChangeEvent<HTMLInputElement>) => setFullName(value)}
+          />
+        </label>
+        <label className="flex flex-col mb-4">
+          Email
+          <input
+            type="text"
+            className="text-black px-2 py-1 rounded-sm"
+            value={email}
+            onChange={({ target: { value }}: ChangeEvent<HTMLInputElement>) => setEmail(value)}
+          />
+        </label>
+        <div className="mt-4">
+          <button
+            className="rounded-full bg-white text-black px-6 py-1 hover:bg-slate-200 text-md"
+            type="submit">
+              Guardar
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
+
+export default Home;
